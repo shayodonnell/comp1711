cefc70d69b495aa91433e424c06679b669ffc260
Shay O'Donnell <115416152+shayodonnell@users.noreply.github.com>
2023-11-17 14:50:31 +0000

    task 2 option A

--- coursework/task2/FitnessDataStruct.h
+++ coursework/task2/FitnessDataStruct.h
@@ -1,50 +1,29 @@
-#ifndef FITNESSDATASTRUCT_H
-#define FITNESSDATASTRUCT_H
-#include <stdio.h>
-#include <stdio.h>
 
-//define fitness data struct
+#ifndef FITNESS_DATA_STRUCT_H
+#define FITNESS_DATA_STRUCT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+// Define an appropriate struct
 typedef struct {
 	char date[11];
 	char time[6];
 	int steps;
 } FITNESS_DATA;
 
-// This is your helper function. Do not change it in any way.
-// Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
-void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
-    // Create a copy of the input string as strtok modifies the string
-    char *inputCopy = strdup(input);
-    
-    // Tokenize the copied string
-    char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
-    }
-    
-    token = strtok(NULL, delimiter);
-    if (token != NULL) {
-        strcpy(time, token);
-    }
-    
-    token = strtok(NULL, delimiter);
-    if (token != NULL) {
-        strcpy(steps, token);
-    }
-    
-    // Free the duplicated string
-    free(inputCopy);
+// Helper function prototypes
+void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
 
+int inputFilename(){
+	char filenameInput[50];
+	printf("Input filename	:\n");
+	scanf("%s", filenameInput);
+	FILE *filepointer;
+	filepointer = fopen(filenameInput, "r");
+	if(filepointer == NULL){
+		printf("Error: Could not open file\n");
+		exit(1);
+	}
 }
 
-int fileread(filenameparam){
-    FILE *file = fopen(filenameparam, "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        exit(1);
-    }
-}
-
-
-#endif
+#endif // FITNESS_DATA_STRUCT_H
--- coursework/task2/StepCounter.c
+++ coursework/task2/StepCounter.c
@@ -1,44 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "FitnessDataStruct.h"
 
-int main()
-{
-    char optionSelected;
-    char filename[50];
-    int status;
+// Struct moved to header file
 
-    printf("Select from the following options:\nA: Specify the filename to be imported\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the data and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Exit\n");
-    scanf("%c", optionSelected);
-    if(optionSelected == "A")
-    {
-        printf("Input filename: ");
-        scanf("%s", filename);
-        status = fileread(filename);
-        if (status == 1) {
-            
-        }
-    }else if (optionSelected == "B")
-    {
-        /* code */
-    }else if (optionSelected == "C")
-    {
-        /* code */
-    }else if (optionSelected == "D")
-    {
+// Define any additional variables here
+// Global variables for filename and FITNESS_DATA array
+char selectedOption;
 
-    }else if (optionSelected == "E")
-    {
 
-    }else if (optionSelected == "F")
-    {
-
-    }else if (optionSelected == "Q")
-    {
-
-    }else
-    {
-        printf("Error: invalid selection");
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
     }
     
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
     
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
+
+
+
+
+// Complete the main function
+int main() {
+   printf("Menu Options:\n");
+   printf("A: Specify the filename to be imported\n");
+   printf("B: Display the total number of records in the file\n");
+   printf("C: Find the date and time of the timeslot with the fewest steps\n");
+   printf("D: Find the data and time of the timeslot with the largest number of steps\n");
+   printf("E: Find the mean step count of all the records in the file\n");
+   printf("F: Find the longest continuous period where the step count is above 500 steps\n");
+   printf("Q: Exit\n");
+   printf("Enter Choice: \n");
+   scanf("%c",&selectedOption);
+
+   switch(selectedOption){
+    case 'A':
+    case 'a':
+        inputFilename();
+        break;
+    case 'B':
+    case 'b':
+        
+        break;
+    case 'C':
+    case 'c':
+        break;
+    case 'D':
+    case 'd':
+        break;
+    case 'E':
+    case 'e':
+        break;
+    case 'F':
+    case 'f':
+        break;
+    case 'Q':
+    case 'q':
+        break;
+   }
 }
+
+
+
+
+
+
+
+
+
